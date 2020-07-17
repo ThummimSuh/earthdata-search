3fb5aa6107bf13e2157a8703b32277b183a3be53
Matthew Crouch <matthew@element84.com>
2020-07-17 15:27:05 -0400

    EDSC-2805: Hides features when portal authentication is disabled

--- static/src/js/util/portals.js
+++ static/src/js/util/portals.js
@@ -8,3 +8,14 @@ export const isDefaultPortal = (portalId) => {
   const defaultPortalId = getApplicationConfig().defaultPortal
   return portalId === defaultPortalId
 }
+
+/**
+ * Returns the value of features.authentication from a portal config
+ * @param {Object} portal Portal config
+ */
+export const authenticationEnabled = (portal) => {
+  const { features = {} } = portal
+  const { authentication } = features
+
+  return authentication
+}
